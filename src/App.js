3b9c6d5df5443b7adfa49b345088c847ad371713
Victor Richtmann <vrichtmann@gmail.com>
2022-05-26 09:21:59 -0300

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 
 import Navegation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
+import SignIn from './routes/sign-in/sign-in.component'
 
 const Shop = () =>{
   return(
@@ -16,6 +17,7 @@ const App = () => {
         {/* <Route path='home' element={<Home />}/> */}
         <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />}/>
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>
 
       {/* /home/shop */}
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,23 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import './navigation.styles.scss'
+
 const Navegation = () => {
     return (
       <Fragment>
         <div className="navigation">
-           <div>Logo</div>
+            <Link className="logo-container" to="/">
+              <CrwnLogo className="logo"/>
+            </Link>
+           
            <div className='nav-links-container'>
                 <Link className="nav-link" to='/shop'>
                     SHOP
+                </Link>
+                <Link className="nav-link" to='/sign-in'>
+                    SIGN IN
                 </Link>
            </div>
         </div>
