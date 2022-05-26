56563c2ebad75213953a125f69ce68fc9e42b1cd
Victor Richtmann <vrichtmann@gmail.com>
2022-05-26 15:30:34 -0300

    Fire base Use effect

---
+++ src/compoments/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,20 @@
+const SignUpForm = () => {
+    return (
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form>
+                <label>Display Name</label>
+                <input required/>
+
+                <label>Email</label>
+                <input required/>
+
+                <label>Password</label>
+                <input required/>
+
+                <label>Confirm Passowrd</label>
+                <input required/>
+            </form>
+        </div>
+    )
+}
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,37 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth'
+
+import {
+    auth,
+    signInWithGooglePopup,
+    // signInWithGoogleRedirect,
+    createUserDocumentFromAuth 
+    } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+
+    // useEffect(() => {
+    //     async function fetchData(){
+    //         const response = await getRedirectResult(auth);
+    //         if(response){
+    //             const userDocRef = await createUserDocumentFromAuth(response.user)
+    //         }
+    //     }
+    //     fetchData();
+    // }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocREf = await createUserDocumentFromAuth(user);
-    }
+    };
 
     return(
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
         </div>
-    )
+    );
 }
 
 export default SignIn;
